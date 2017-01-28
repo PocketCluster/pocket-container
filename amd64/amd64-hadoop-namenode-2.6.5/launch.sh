c1ee9948df9b0f15cc3b011999c1c57766d23d23
stkim1 <stkim1@colorfulglue.com>
2017-01-28 19:02:43 +0900

    multihomed network

--- amd64/amd64-hadoop-namenode-2.6.5/launch.sh
+++ amd64/amd64-hadoop-namenode-2.6.5/launch.sh
@@ -9,6 +9,7 @@ if [[ -z ${CLUSTER_NAME} ]]; then
 	echo "unknown cluster name"
 	exit 2
 fi
+
 if [[ -z ${SLAVE_NODES} ]]; then
 	echo "unknown slave nodes"
 	exit 2
@@ -17,6 +18,7 @@ fi
 if [[ -f ${HADOOP_SLAVE} ]]; then
 	rm ${HADOOP_SLAVE}
 fi
+
 for slave in ${SLAVE_NODES//;/ }
 do
 	host=(${slave//=/ })
@@ -28,16 +30,13 @@ do
 done
 
 # check hdfs format
-if [ "`ls -A /pocket`" == "" ]; then
-  echo "Formatting namenode name directory: /pocket"
-  mkdir -p /pocket/hdfs/namenode
-  mkdir -p /pocket/hdfs/namenode2-checkpoint
-  mkdir -p /pocket/log
-  ${HADOOP_HOME}/bin/hdfs --config ${HADOOP_CONF_DIR} namenode -format ${CLUSTER_NAME}
+if [ ! -d /pocket/hdfs/namenode ] || [ ! -d /pocket/hdfs/namenode2-checkpoint ] || [ ! -d /pocket/log ]; then
+    echo "Formatting namenode name directory: /pocket"
+    mkdir -p /pocket/hdfs/namenode
+    mkdir -p /pocket/hdfs/namenode2-checkpoint
+    mkdir -p /pocket/log
+    ${HADOOP_HOME}/bin/hdfs --config ${HADOOP_CONF_DIR} namenode -format ${CLUSTER_NAME}
 fi
-
-# start sshd service
-service ssh start
 
 ${HADOOP_HOME}/sbin/start-dfs.sh
 ${HADOOP_HOME}/bin/hdfs dfsadmin -safemode wait
--- armhf/armhf-hadoop-datanode-2.6.5/launch.sh
+++ armhf/armhf-hadoop-datanode-2.6.5/launch.sh
@@ -4,15 +4,13 @@ source /root/.bashrc
 set -f
 
 HADOOP_SLAVE="/etc/hadoop/slaves"
+
 if [[ -f ${HADOOP_SLAVE} ]]; then
 	rm ${HADOOP_SLAVE}
 fi
 
-# check hdfs structure
-if [ "`ls -A /pocket`" == "" ]; then
-  echo "Building datanode structure"
-  mkdir -p /pocket/hdfs/namenode
-  mkdir -p /pocket/hdfs/namenode2-checkpoint
-  mkdir -p /pocket/hdfs/datanode
-  mkdir -p /pocket/log
+if [ ! -d /pocket/hdfs/datanode ] || [ ! -d /pocket/log ]; then
+    echo "Building datanode structure"
+    mkdir -p /pocket/hdfs/datanode
+    mkdir -p /pocket/log
 fi
