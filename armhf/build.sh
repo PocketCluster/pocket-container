d29cea188b44508cda379e05a51f4d98f640765a
stkim1 <stkim1@colorfulglue.com>
2017-02-01 00:07:22 +0900

    fix spark

--- armhf/build.sh
+++ armhf/build.sh
@@ -211,8 +211,8 @@ function build_spark_slave() {
 }
 
 
-#build_baseimage
-#build_zulu_jdk
-#build_hadoop_base 0
-#build_hadoop_datanode 0
+build_baseimage
+build_zulu_jdk
+build_hadoop_base 0
+build_hadoop_datanode 0
 build_spark_slave 0
