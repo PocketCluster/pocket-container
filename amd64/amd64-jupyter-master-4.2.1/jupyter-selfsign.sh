9e392a0164ea10ad79fd2559e316193549d13a15
stkim1 <stkim1@colorfulglue.com>
2017-02-08 23:50:05 +0900

    jupyter setup

--- amd64/amd64-jupyter-master-4.2.1/jupyter-selfsign.sh
+++ amd64/amd64-jupyter-master-4.2.1/jupyter-selfsign.sh
@@ -1,5 +1,6 @@
-#/usr/bin/env bash
+#!/bin/sh
 
-if [ ! -f /usr/local/share/jupyter/certs/pc-core-jupyter.key ] || [ ! -f /usr/local/share/jupyter/certs/pc-core-jupyter.pem ]; then
-    /usr/bin/openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /usr/local/share/jupyter/certs/pc-core-jupyter.key -out /usr/local/share/jupyter/certs/pc-core-jupyter.pem -subj "/O=PocketCluster/CN=pc-core" || true
+if [ ! -d /usr/local/share/jupyter/certs/ ] || [ ! -f /usr/local/share/jupyter/certs/pc-core-jupyter.key ] || [ ! -f /usr/local/share/jupyter/certs/pc-core-jupyter.pem ]; then
+    mkdir -p /usr/local/share/jupyter/certs/ 
+    /usr/bin/openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /usr/local/share/jupyter/certs/pc-core-jupyter.key -out /usr/local/share/jupyter/certs/pc-core-jupyter.pem -subj "/O=PocketCluster/CN=pc-core"
 fi
