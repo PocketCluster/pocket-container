e33c15b09c9b82ba661f4770e4a02c89f285fbb9
stkim1 <stkim1@colorfulglue.com>
2018-01-28 00:17:10 +0900

    x86_64 baseimage fix

--- x86_64/x86_64-baseimage/services/sshd/sshd.sh
+++ x86_64/x86_64-baseimage/services/sshd/sshd.sh
@@ -9,17 +9,12 @@ SSHD_BUILD_PATH=/bd_build/services/sshd
 $minimal_apt_get_install openssh-server
 mkdir /var/run/sshd
 mkdir /etc/service/sshd
-touch /etc/service/sshd/down
+
 cp $SSHD_BUILD_PATH/sshd.runit /etc/service/sshd/run
 cp $SSHD_BUILD_PATH/sshd_config /etc/ssh/sshd_config
 cp $SSHD_BUILD_PATH/00_regen_ssh_host_keys.sh /etc/my_init.d/
 
-## Install default SSH key for root and app.
+## Make default SSH key path
 mkdir -p /root/.ssh
 chmod 700 /root/.ssh
 chown root:root /root/.ssh
-cp $SSHD_BUILD_PATH/keys/insecure_key.pub /etc/insecure_key.pub
-cp $SSHD_BUILD_PATH/keys/insecure_key /etc/insecure_key
-chmod 644 /etc/insecure_key*
-chown root:root /etc/insecure_key*
-cp $SSHD_BUILD_PATH/enable_insecure_key /usr/sbin/
