58d1a1e436e2963d262c5b70d4fd4fac0af88118
stkim1 <stkim1@colorfulglue.com>
2018-01-30 13:55:09 +0900

    java env fix

--- aarch64/aarch64-hadoop-base-2.7.3/etc-hadoop/hadoop-env.sh
+++ aarch64/aarch64-hadoop-base-2.7.3/etc-hadoop/hadoop-env.sh
@@ -26,7 +26,7 @@
 # remote nodes.
 
 # The java implementation to use.
-export JAVA_HOME="/opt/jdk"
+export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-arm64"
 
 # The jsvc implementation to use. Jsvc is required to run secure datanodes.
 #export JSVC_HOME=${JSVC_HOME}
